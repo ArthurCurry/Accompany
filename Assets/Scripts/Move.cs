2510a861513f01fec2b99df1595a9090799601dd
wxj <825754896@qq.com>
2019-12-13 22:43:09 +0800

    修改

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -22,8 +22,8 @@ public class Move : MonoBehaviour {
 
     void UpdatePosition()
     {
-        Vector3 borderRight = Camera.main.ScreenToWorldPoint( new Vector3(Screen.width,Screen.height/2,-10));
-        Vector3 borderLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height / 2, -10));
+        Vector3 borderRight = Camera.main.ScreenToWorldPoint( new Vector3(Screen.width,Screen.height/2,10));
+        Vector3 borderLeft = Camera.main.ScreenToWorldPoint(new Vector3(0, Screen.height / 2, 10));
         if(right>0)
         {
             transform.position = (gapObj.transform.position + Vector3.right * gap.width/2 + borderRight) / 2;
@@ -31,6 +31,19 @@ public class Move : MonoBehaviour {
         else
         {
             transform.position = (gapObj.transform.position + Vector3.left * gap.width/2 + borderLeft) / 2;
+        }
+    }
+
+   
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.tag.Equals("Thing"))
+        {
+            gap.UpdateTargetSize(true);
+        }
+        else if(collider.gameObject.tag.Equals(""))
+        {
+            gap.UpdateTargetSize(false);
         }
     }
 }
