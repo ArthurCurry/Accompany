115d7462ea6d3dd031d29514aa2cdecd543275df
zszhe <39942855+zszhe@users.noreply.github.com>
2019-12-13 23:02:00 +0800

    移动模式的改变。

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,15 +8,57 @@ public class Player : MonoBehaviour {
 
     public Transform pos;
 
-	// Use this for initialization
-	void Start () {
+    public Transform center;
+
+    //
+    public float _radius_length;
+    public float _angle_speed;
+
+    private float temp_angle;
+
+    private Vector3 _pos_new;
+
+    public Vector3 _center_pos;
+
+    public bool _round_its_center;
+    // Use this for initialization
+    void Start () {
         speed = 10;
-	}
+        _center_pos = center.transform.position;
+        _angle_speed = 4f;
+        _radius_length = Vector3.Distance(transform .position, center.position);
+
+        temp_angle = -Mathf.Deg2Rad * Vector2.Angle((transform .position -center .position),transform.right );
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        Move();
+        Move1();
 	}
+
+    void Move1() //圆周运动
+    {
+        if(Input .GetKey(KeyCode.Q))
+        {
+            temp_angle += _angle_speed * Time.deltaTime;
+            _pos_new.x = _center_pos.x + Mathf.Cos(temp_angle) * _radius_length;
+            _pos_new.y = _center_pos.y + Mathf.Sin(temp_angle) * _radius_length;
+            _pos_new.z = transform.localPosition.z;
+
+            transform.localPosition = _pos_new;
+        }
+        else if(Input .GetKey(KeyCode.E))
+        {
+            temp_angle -= _angle_speed * Time.deltaTime;
+            _pos_new.x = _center_pos.x + Mathf.Cos(temp_angle) * _radius_length;
+            _pos_new.y = _center_pos.y + Mathf.Sin(temp_angle) * _radius_length;
+            _pos_new.z = transform.localPosition.z;
+
+            transform.localPosition = _pos_new;
+        }
+        
+    }
 
     void Move()
     {
